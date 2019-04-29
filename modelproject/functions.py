bb63b23f29e45efee8440b0ea48a384d59db4d27
Walschuppli <42266445+Walschuppli@users.noreply.github.com>
2019-04-29 16:02:04 +0200

    Simple model done

--- modelproject/functions.py
+++ modelproject/functions.py
@@ -22,6 +22,17 @@ def best_response(q2, c1, a, b): #best response for firm 1 - found by maximising
                             disp=True)
     return q1[0]
 
+def best_reponse_plot(a, b, c1): #plots the best response function
+    from scipy import optimize, arange
+    import matplotlib.pyplot as plt
+    
+    range_q2 = arange(0, a, 0.01)
+    range_q1 = [best_response(q2, c1, a, b) for q2 in range_q2]
+    
+    plt.plot(range_q1, range_q2)
+    plt.xlabel("best response q1")
+    plt.ylabel('q2')
+    plt.title("Firm 1's best response to firm 2")
 
 
 def help_br(q, c, a, b): #defining a help function that finds where the Best response functions - q are equal to 0 (i.e. fixed points)
@@ -29,5 +40,16 @@ def help_br(q, c, a, b): #defining a help function that finds where the Best res
 
 
 def production_eq(c, initial_guess, a, b): #Equilibrium function 
+    from scipy import optimize
     Q = optimize.fsolve(lambda q: help_br(q, c, a, b), initial_guess)
     return Q
+
+
+def summary(c, a, b, initial_guess):
+    q1,q2 = production_eq(c, initial_guess, a, b) #Equilibrium productions   
+     
+    output = print('\n Industry output is ' + str("{:.0f}".format(q1+q2)) + ' with firm 1 producing ' + str("{:.0f}".format(q1)) + ' units and firm 2 producing ' +
+                   str("{:.0f}".format(q2)) + ' units.' + 
+                   '\n The equilibrium price per unit becomes ' + str("{:.0f}".format(price(q1, q2, a, b))) +
+                  "\n Firm 1's profit will be " + str("{:.0f}".format(profit(q1, q2, a, b, c[0]))) + " while firm 2's profit will be " + str("{:.0f}".format(profit(q1, q2, a, b, c[1]))))
+    return output
